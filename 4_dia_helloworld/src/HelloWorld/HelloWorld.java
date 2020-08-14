776ba1005f6ae6732a39dc3ae4527acb20c56dda
Gabriel Silva Andrade <34127765+Gabrielsilvaa@users.noreply.github.com>
2020-08-14 11:01:33 -0300

    frist project 100 days

---
+++ 4_dia_helloworld/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+package HelloWorld;
+
+//Nossa classe Helloworld
+public class HelloWorld {
+
+    //Nosso metodo main ele é o responsavel por começar a rodar nosso projeto
+    //nos podemos escrever ele de forma rapida usando apenas a inicial de cada palavra
+    // exemplo se voce colocar psvm e der enter ja vai escrever o metodo abaixo
+    public static void main(String[] args) {
+
+        //aqui temos nosso print, responsavel por realizar impressao do texto hello world
+        // tambem podemos simplificar a escrita podemos colocar sout e vai trazer todo texto
+        //e para realizar impressao de textos temos que colocar aspas dupla e depois o texto
+        System.out.println("Hello World Java!");
+
+    }
+}
