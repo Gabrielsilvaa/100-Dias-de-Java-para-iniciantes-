a84f902699bd2e1dfe3e74f8a4d61e9de23d283f
Gabriel Silva Andrade <34127765+Gabrielsilvaa@users.noreply.github.com>
2020-08-16 14:38:48 -0300

    6 dia - tipos de dados

---
+++ 6_tipos_de_dados/src/tipodedados/TipoDados.java
@@ -0,0 +1,41 @@
+package tipodedados;
+
+public class TipoDados {
+
+    public static void main(String[] args) {
+        //Declarnado um Bloolean
+        //valor que recebe verdadeiro ou falso
+        boolean verdade = true;
+        boolean mentira = false;
+
+        //Declarando um Byte
+        // o byte  vai de -128 a 127
+        byte idade = 18;
+
+        //Declarando um Short
+        //O short vai de -32768 a 32767
+        short ano = 18;
+
+        //Declarando um Int
+        //o int vai de -2147483648 a 2147483647
+        int milenio = 18;
+
+        //Declarando um Long
+        //O long vai de -9223372036854775808 a 9223372036854775807
+        long pessoas = 18000L;
+
+        //Declarando um Float
+        //muito usado para medir temperatura tem precisao de 6 a 7 casas decimais
+        float temperatura = 22.5F;
+
+        //Declarando um Double
+        //muito usado para medir ou calcular distancias tem precisao de 15 a 16 casas decimais
+        double distancia = 1.5;
+
+        //Declarando um Char
+        //usado para guardar um unico caracter
+        char sexo = 'M';
+
+    }
+
+}
