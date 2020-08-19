88c3b3f4b7a4c7ca2671652fbfd1e1b335eaed6a
Gabriel Silva Andrade <34127765+Gabrielsilvaa@users.noreply.github.com>
2020-08-19 17:32:23 -0300

    Operadores Lógicos

---
+++ 9_operadores_logicos/src/operadores/Operadoreslogicos.java
@@ -0,0 +1,23 @@
+package operadores;
+
+public class Operadoreslogicos {
+
+    public static void main(String[] args) {
+
+        // && - operador E é delcador com E comercial
+        // || - operador OU e declarado com ||
+        // ! - operador NOT e declarado com !
+
+        //Com o operador lógico E (&&) testamos se dois booleanos são true.
+        System.out.println(true&&true);
+
+        //O operador OU (||) testa se pelo menos um dentre dois booleanos é true.
+        System.out.println(false||true);
+
+        //NOT (!) nega um booleano, que é equivalente a inverter o seu valor
+        System.out.println(!true + " not");
+
+
+    }
+
+}
