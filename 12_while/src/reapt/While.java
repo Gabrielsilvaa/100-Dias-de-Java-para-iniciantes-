e18fe67d229e6876ffd52499fa7e4dae2c722040
Gabriel Silva Andrade <34127765+Gabrielsilvaa@users.noreply.github.com>
2020-08-22 15:48:58 -0300

    while

---
+++ 12_while/src/reapt/While.java
@@ -0,0 +1,18 @@
+package reapt;
+
+public class While {
+    public static void main(String[] args) {
+
+        //definimos uma variavel contador
+        Integer cont = 0;
+
+        //Iniciamos nosso bloco de repetição e passamos uma condição para ele executar
+        while (cont <=10){
+            //imprimimos o valor da variavel para acompanhar sua repetição
+            System.out.println(cont);
+            //adiciona mais um
+            cont++;
+        }
+
+    }
+}
