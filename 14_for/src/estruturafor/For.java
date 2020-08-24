c44cbbebb09577bdfebd1a410987202a751a5964
Gabriel Silva Andrade <34127765+Gabrielsilvaa@users.noreply.github.com>
2020-08-24 18:21:57 -0300

    FOR

---
+++ 14_for/src/estruturafor/For.java
@@ -0,0 +1,21 @@
+
+
+
+package estruturafor;
+
+public class For {
+
+    public static void main(String[] args) {
+
+        //declaramos a variavel dentro do for mas tambem podemos chamar ela
+        //depois passamos a condição
+        //e por ultimo o que deverar fazer
+
+       for (Integer mem=0; mem < 2; mem++){
+        //comando que sera repetido
+           System.out.println(mem);
+       }
+
+    }
+
+}
