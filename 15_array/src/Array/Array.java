ecd2706613c173350db224b41e0a52ac5c776ec6
Gabriel Silva Andrade <34127765+Gabrielsilvaa@users.noreply.github.com>
2020-08-25 18:10:32 -0300

    Array

---
+++ 15_array/src/Array/Array.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class Array {
+    public static void main(String[] args) {
+
+        Integer array[] = {10, 50, 30, 52, 30}; //Nosso array
+        Integer n = 0; //variavel para percorrer a index
+
+        //while para percorrer todas index
+        while (n <= 4){
+            System.out.println(array[n]);
+            n ++;
+        }
+
+
+    }
+}
