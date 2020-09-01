0147c0612a2fbd09f6f2fad21b18d5aa0362302e
Gabriel Silva Andrade <34127765+Gabrielsilvaa@users.noreply.github.com>
2020-09-01 19:05:09 -0300

    Ola mundo com POO

---
+++ 22_helloworld_with_poo/src/helloworld/poo/HelloWorld.java
@@ -0,0 +1,11 @@
+package helloworld.poo;
+
+//Ciação da class
+public class HelloWorld {
+
+    //Criação do metodo
+    public void sayHello() {
+        // Imprime a mensagem Hello World! no terminal
+        System.out.println("Hello World!");
+    }
+}
---
+++ 22_helloworld_with_poo/src/helloworld/poo/Program.java
@@ -0,0 +1,12 @@
+package helloworld.poo;
+
+public class Program {
+
+    public static void main(String[] args) {
+        // Cria uma instância da classe HelloWorld
+        HelloWorld helloWorld = new HelloWorld();
+        // Invoca o método sayHello() usando o objeto
+        // helloWorld para isso
+        helloWorld.sayHello();
+    }
+}
