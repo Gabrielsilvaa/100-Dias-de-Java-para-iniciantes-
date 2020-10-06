a96069a6aab7e00402ff65edd94e60cf401361dd
Gabriel Silva Andrade <34127765+Gabrielsilvaa@users.noreply.github.com>
2020-10-06 20:50:21 -0300

    Array List

---
+++ 57_arraylist/src/arraylist/Main.java
@@ -0,0 +1,17 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //criamos nosso obj array
+        ArrayList <String> teste = new ArrayList<>();
+        //adicionamos intens no nosso array
+        teste.add("Melancia");
+
+
+        System.out.println(teste);
+
+    }
+}
